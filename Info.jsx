e0ff552897f0c76499f8e820a0a0ca0476b66935
shriya25-b <85452125+shriya25-b@users.noreply.github.com>
2021-06-27 14:15:44 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+    return (
+      <div class="note">
+       <h1> Javascript and React.js </h1>
+       <p> A basic web dev React Js 7 Days Bootcamps</p>
+      </div>
+    );
+}
+
+export default Info;
